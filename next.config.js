2d9653a1192d41fbe5d184e6b665e179e179ce40
mike <namike623@gmail.com>
2023-06-26 22:59:42 +0100

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
